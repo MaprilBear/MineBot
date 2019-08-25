c54731e8b76564dded5232da7431f63179a69cd1
April <pcmasterrace0805@gmail.com>
2019-08-24 23:59:33 -0500

    Added %whois
    Returns the discord tag of a registered user with a matching UUID of the provided name
    
    Updated help text

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Command = require("./Command");
+const mojang = require('mojang-api');
 const InputCommand = require("./InputCommand");
 const {JSDOM} = require("jsdom");
 const { window } = new JSDOM();
@@ -11,11 +12,14 @@ var ip = "66.235.174.205:25580";
 var prefix;
 var helpText =
     "General:\n" +
-    "- %player - lists all players connected to the server\n" +
+    "- %status - lists all players connected to the server\n" +
     "- %ip - returns the server's IP address\n" +
+    "- %motd - returns the server's message of the day\n" +
+    "- %whois [name] - returns the discord tag of a Minecraft player provided they have been registered on any server \n" +
     "\n" +
     "Administrative:\n" +
-    "- %setip - changes the server's ip address (You must set this value before using any commands that require a server to be set)";
+    "- %setip [ip] - changes the server's ip address (You must set this value before using any commands that require a server to be set) \n" +
+    "- %setprefix [prefix] - changes the bot's prefix for commands (cannot be $)";
 
 //MongoDB
 const uri = process.argv[3];
@@ -52,7 +56,7 @@ const statusCommand = new Command("status", function (msg) {
             }
             $.each(status.players.list, function (index, player) {
                 console.log(player);
-                playerList += player + " ";
+                playerList += player + ", ";
             });
 
             //Show a list of plugins
@@ -64,7 +68,7 @@ const statusCommand = new Command("status", function (msg) {
 
              */
 
-            msg.reply("The server is ONLINE :white_check_mark: running version " + status.version + " with " + playerList);
+            msg.reply("The server is ONLINE :white_check_mark: running version " + status.version + " with " + playerList.substring(0, playerList.length - 2));
         }
     });
 });
@@ -115,6 +119,38 @@ const setPrefixCommand = new Command('setprefix', function (msg) {
     }
 });
 
+
+
+const whoIsCommand = new Command('whois', function (msg) {
+
+    try {
+        mojang.nameToUuid(msg.content.split(" ")[1], function (err, result) {
+            if (result === null || result === undefined){
+                msg.reply("Invalid name (Misspelled or not registed");
+            } else {
+                console.log("Name: " + result[0].name);
+                console.log("UUID: " + result[0].id);
+
+                DirtDB.collection('Players').findOne({uuid: result[0].id}, function (err, result) {
+
+                    var discordID = result.discordid;
+                    DiscordClient.fetchUser(discordID).then(r => msg.reply(r.tag));
+
+                    //IF MENTIONS
+                    /*
+                    console.log("Discord ID: " + discordID);
+                    msg.reply("<@" + discordID + ">").then(sent => console.log(sent.mentions.users.array()[0].tag));
+
+                     */
+
+                })
+            }
+        });
+    } catch (err){
+        msg.reply("Invalid name (Misspelled or not registed");
+    }
+});
+
 //End of Commands
 
 var started = false;
@@ -141,6 +177,8 @@ DiscordClient.on('message', msg => {
     //Check for commands
     if(msg.author === DiscordClient.user){}else {
 
+
+
         console.log(msg.author.id);
 
         //Connect to Mongo and setup ip and prefix
@@ -149,6 +187,21 @@ DiscordClient.on('message', msg => {
             ip = result.ip;
             console.log(prefix);
             console.log(ip);
+
+            //register
+            if(msg.channel.id === "610608225246511131" && RegExp(prefix + "register .+")){
+                DirtDB.collection("Settings").findOne({discordid: msg.author.id}, function (err, result) {
+                    if (result == null){
+                        DirtDB.collection("Settings").insertOne({uuid: mojang.nameToUuid(msg.content.split(" ")[1].id), discordid: msg.author.id}, undefined, function () {
+                            msg.reply("Your name has been successfully registered. If this was a mistake please contact this Bot's author Panda#4724");
+                        })
+                    } else {
+                        msg.reply('Your name has already been registered')
+                    }
+                });
+                msg.reply("This channel is for registering Minecraft usernames only");
+                msg.delete();
+            }
 
             if (helpCommand.getRegex(prefix).test(msg.content)) {
                 console.log("help");
@@ -178,8 +231,13 @@ DiscordClient.on('message', msg => {
                 setPrefixCommand.onCall(msg);
             } else if(msg.author.id === "183958085763727360" && msg.content === "!@#forcequit"){
                 mongoClient.close(function () {
-                    process.exit(0);
+                    process.exit();
                 })
+            } else if (whoIsCommand.getRegex(prefix).test(msg.content)){
+                console.log("whoIs command:");
+                whoIsCommand.onCall(msg);
+            } else if(msg.content.substring(0,1) === prefix){
+                msg.reply("Invalid command or argument (Check for misspellings or missing argument");
             }
         })
     }
@@ -192,6 +250,10 @@ DiscordClient.login(token);
 process.on('SIGINT', function() {
     console.log("Caught interrupt signal");
     mongoClient.close(true, function () {
-        process.exit(0);
+        process.exit();
     });
 });
+
+
+
+
